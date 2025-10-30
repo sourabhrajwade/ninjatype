eed2890c2acc217806ce8eee1e053cd5b2a79502
vivek <vivekgpt107@gmail.com>
2025-10-30 11:27:57 +0530

    added reset button

--- src/components/keyboard/ResetButton.tsx
+++ src/components/keyboard/ResetButton.tsx
@@ -1,9 +1,19 @@
+import { $config } from "@/store/config";
 import { Icon } from "@iconify/react";
+import { useStore } from "@nanostores/react";
 
 const ResetButton = () => {
-    return ( <div className="btn">
-        <Icon icon="solar:restart-linear" width="24" height="24" />
-    </div> );
-}
- 
+    const config = useStore($config);
+    const handleReset = () => {
+        $config.set({...config});
+    };
+    return (
+        <div id="reset-button">
+            <button className="btn" onClick={handleReset} title="Reset Configuration">
+                <Icon icon="solar:restart-linear" width="24" height="24" />
+            </button>
+        </div>
+    );
+};
+
 export default ResetButton;
